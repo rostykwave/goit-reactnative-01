3c71627383d62d8121ec777b59496201ec4c7c8c
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-08-14 16:43:25 +0300

    camera

--- screens/mainScreen/CreateScreen.js
+++ screens/mainScreen/CreateScreen.js
@@ -1,15 +1,30 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
 
 const CreateScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>CreateScreen</Text>
+      <Camera style={styles.camera}>
+        <TouchableOpacity onPress={() => {}} style={styles.snapContainer}>
+          <Text style={styles.snap}>SNAP</Text>
+        </TouchableOpacity>
+      </Camera>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  container: { flex: 1 },
+  camera: { height: 300, alignItems: 'center' },
+  snap: { color: '#fff' },
+  snapContainer: {
+    marginTop: 200,
+    borderWidth: 1,
+    borderColor: '#fff',
+    width: 70,
+    height: 70,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 export default CreateScreen;
